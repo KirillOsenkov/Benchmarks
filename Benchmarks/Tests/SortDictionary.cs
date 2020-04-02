35e8ac7130be58986b6abab8c2686e60a6e9e0c6
Kirill Osenkov <github@osenkov.com>
2020-04-02 16:52:36 -0700

    Fix merge conflicts, order test methods.

--- Benchmarks/Tests/SortDictionary.cs
+++ Benchmarks/Tests/SortDictionary.cs
@@ -4,6 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 
+/*
+|                        Method |       Mean |    Error |   StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|------------------------------ |-----------:|---------:|---------:|-------:|------:|------:|----------:|
+| SortInPlaceCopyToKeyValuePair |   757.7 ns |  5.02 ns |  4.69 ns | 0.0210 |     - |     - |      88 B |
+|         SortedListConstructor |   912.2 ns |  9.45 ns |  7.38 ns | 0.0381 |     - |     - |     160 B |
+|                 SortedListAdd |   987.3 ns |  7.37 ns |  6.90 ns | 0.0381 |     - |     - |     160 B |
+|       SortInPlaceKeyValuePair | 1,043.0 ns | 19.18 ns | 16.02 ns | 0.0267 |     - |     - |     112 B |
+|             SortInPlaceLambda | 1,043.8 ns | 10.67 ns |  9.98 ns | 0.0267 |     - |     - |     112 B |
+|        SortInPlaceMethodGroup | 1,052.1 ns |  5.81 ns |  5.43 ns | 0.0343 |     - |     - |     144 B |
+|              SortedDictionary | 2,241.1 ns |  7.65 ns |  5.97 ns | 0.0916 |     - |     - |     389 B |
+|            SortWhileProducing | 2,712.3 ns | 16.36 ns | 15.30 ns | 0.0153 |     - |     - |      68 B |
+|                    Enumerable | 2,900.2 ns | 20.54 ns | 17.15 ns | 0.0229 |     - |     - |     108 B |
+|                       OrderBy | 4,197.6 ns | 48.90 ns | 45.74 ns | 0.0610 |     - |     - |     260 B |
+ */
+
 namespace Tests
 {
     [MemoryDiagnoser]
@@ -21,58 +36,62 @@ namespace Tests
             { "PathInPackage", "lib/netstandard2.0/ICSharpCode.Decompiler.dll" },
         };
 
-        // 4.160 us, 260 bytes allocated
         [Benchmark]
-        public void OrderBy()
+        public void SortInPlaceCopyToKeyValuePair()
         {
-            var result = dictionary.OrderBy(kvp => kvp.Key);
-            foreach (var item in result)
-            {
-            }
-        }
+            var list = new KeyValuePair<string, string>[dictionary.Count];
 
-        [Benchmark]
-        public void SortedDictionary()
-        {
-            var result = new SortedDictionary<string, string>(dictionary);
-            foreach (var item in result)
-            {
-            }
-        }
+            ((ICollection<KeyValuePair<string, string>>)dictionary).CopyTo(list, 0);
 
-        [Benchmark]
-        public void SortedList()
-        {
-            var result = new SortedList<string, string>(dictionary);
-            foreach (var item in result)
-            {
-            }
-        }
-
-        private int Comparer((string, string) left, (string, string) right)
-        {
-            return StringComparer.OrdinalIgnoreCase.Compare(left.Item1, right.Item1);
-        }
-
-        // 1.048 us, 144 bytes allocated
-        [Benchmark]
-        public void SortInPlaceMethodGroup()
-        {
-            var list = new List<(string key, string value)>(dictionary.Count);
-
-            foreach (var kvp in dictionary)
-            {
-                list.Add((kvp.Key, kvp.Value));
-            }
-
-            list.Sort(Comparer);
+            Array.Sort(list, (l, r) => string.Compare(l.Key, r.Key, StringComparison.OrdinalIgnoreCase));
 
             foreach (var kvp in list)
             {
             }
         }
 
-        // 1.040 us, 112 bytes allocated
+        [Benchmark]
+        public void SortedListConstructor()
+        {
+            var list = new SortedList<string, string>(dictionary, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var kvp in list)
+            {
+            }
+        }
+
+        [Benchmark]
+        public void SortedListAdd()
+        {
+            var list = new SortedList<string, string>(dictionary.Count, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var kvp in dictionary)
+            {
+                list.Add(kvp.Key, kvp.Value);
+            }
+
+            foreach (var kvp in list)
+            {
+            }
+        }
+
+        [Benchmark]
+        public void SortInPlaceKeyValuePair()
+        {
+            var list = new List<KeyValuePair<string, string>>(dictionary.Count);
+
+            foreach (var kvp in dictionary)
+            {
+                list.Add(kvp);
+            }
+
+            list.Sort((l, r) => StringComparer.OrdinalIgnoreCase.Compare(l.Key, r.Key));
+
+            foreach (var kvp in list)
+            {
+            }
+        }
+
         [Benchmark]
         public void SortInPlaceLambda()
         {
@@ -90,22 +109,35 @@ namespace Tests
             }
         }
 
-        // 1.037 us, 112 bytes allocated
         [Benchmark]
-        public void SortInPlaceKeyValuePair()
+        public void SortInPlaceMethodGroup()
         {
-            var list = new List<KeyValuePair<string, string>>(dictionary.Count);
+            var list = new List<(string key, string value)>(dictionary.Count);
 
             foreach (var kvp in dictionary)
             {
-                list.Add(kvp);
+                list.Add((kvp.Key, kvp.Value));
             }
 
-            list.Sort((l, r) => StringComparer.OrdinalIgnoreCase.Compare(l.Key, r.Key));
+            list.Sort(Comparer);
 
             foreach (var kvp in list)
             {
             }
+        }
+
+        [Benchmark]
+        public void SortedDictionary()
+        {
+            var result = new SortedDictionary<string, string>(dictionary);
+            foreach (var item in result)
+            {
+            }
+        }
+
+        private int Comparer((string, string) left, (string, string) right)
+        {
+            return StringComparer.OrdinalIgnoreCase.Compare(left.Item1, right.Item1);
         }
 
         [Benchmark]
@@ -210,6 +242,15 @@ namespace Tests
 
                     yield return buffer[buffer.Length - 1];
                 }
+            }
+        }
+
+        [Benchmark]
+        public void OrderBy()
+        {
+            var result = dictionary.OrderBy(kvp => kvp.Key);
+            foreach (var item in result)
+            {
             }
         }
     }
