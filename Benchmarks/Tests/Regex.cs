00dbd52d0893549ac0527d4affdeac5e071e16d5
Kirill Osenkov <github@osenkov.com>
2021-04-28 21:00:12 -0700

    Add CultureInvariant test.

--- Benchmarks/Tests/Regex.cs
+++ Benchmarks/Tests/Regex.cs
@@ -6,14 +6,15 @@ using BenchmarkDotNet.Attributes;
 namespace Tests
 {
     /*
-    |            Method |     Mean |     Error |    StdDev |
-    |------------------ |---------:|----------:|----------:|
-    |            Regex1 | 9.147 us | 0.0619 us | 0.0549 us |
-    |     RegexCompiled | 4.055 us | 0.0333 us | 0.0260 us |
-    |            Regex3 | 6.049 us | 0.0383 us | 0.0358 us |
-    |            Regex4 | 3.573 us | 0.0304 us | 0.0285 us |
-    |            Regex5 | 1.418 us | 0.0116 us | 0.0108 us |
-    | RegexMatchSuccess | 1.423 us | 0.0103 us | 0.0096 us |
+    |                Method |     Mean |     Error |    StdDev |
+    |---------------------- |---------:|----------:|----------:|
+    |                Regex1 | 9.163 us | 0.1135 us | 0.0886 us |
+    |         RegexCompiled | 4.196 us | 0.0364 us | 0.0322 us |
+    |                Regex3 | 6.066 us | 0.0409 us | 0.0342 us |
+    |                Regex4 | 3.557 us | 0.0282 us | 0.0250 us |
+    |                Regex5 | 1.363 us | 0.0081 us | 0.0076 us |
+    |     RegexMatchSuccess | 1.486 us | 0.0297 us | 0.0520 us |
+    | RegexCultureInvariant | 1.366 us | 0.0099 us | 0.0093 us |
     */
     public class Regexes
     {
@@ -22,6 +23,7 @@ namespace Tests
         private Regex regex3 = new Regex("Task \".*?\" skipped, due to false condition; (.*?) was evaluated as (.*?).");
         private Regex regex4 = new Regex(@"Task "".*?"" skipped, due to false condition; \(.*?\) was evaluated as \(.*?\)\.");
         private Regex regex5 = new Regex(@"Task "".*?"" skipped, due to false condition; \(.*?\) was evaluated as \(.*?\)\.", RegexOptions.Compiled);
+        private Regex regex6 = new Regex(@"Task "".*?"" skipped, due to false condition; \(.*?\) was evaluated as \(.*?\)\.", RegexOptions.Compiled | RegexOptions.CultureInvariant);
         private string text = @"Task ""Error"" skipped, due to false condition; (('$(CurrentSolutionConfigurationContents)' == '') and ('$(SkipInvalidConfigurations)' != 'true')) was evaluated as (('<SolutionConfiguration>('' != 'true')).";
 
         [Benchmark]
@@ -58,6 +60,12 @@ namespace Tests
         public void RegexMatchSuccess()
         {
             _ = regex5.Match(text).Success;
+        }
+
+        [Benchmark]
+        public void RegexCultureInvariant()
+        {
+            _ = regex6.IsMatch(text);
         }
     }
 }
