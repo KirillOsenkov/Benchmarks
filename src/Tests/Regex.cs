f8acca67c0a69964eb576154cb7cbd38f58d49ef
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2021-06-02 13:45:15 +0300

    Updated tests regarding to modern Conari 1.5 https://github.com/3F/Conari/releases/tag/1.5

--- src/Tests/Regex.cs
+++ src/Tests/Regex.cs
@@ -1,9 +1,9 @@
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using BenchmarkDotNet.Attributes;
 using net.r_eg.Conari;
+using net.r_eg.Conari.Core;
 using net.r_eg.Conari.Types;
 
 namespace Tests
@@ -13,49 +13,56 @@ namespace Tests
             Since preparing adds extra work to the compiler. 
             For example, 
 
-                Regex6CultureInvariant    |   1,770.9 ns
+                Regex7Compiled | 1,727.3 ns
               +
-                PrepareOnce_RegexCompiled | 128,166.4 ns
-              = mean 129937.3 ns (1 Nanosecond (0.000000001 sec))
+                PrepareOnce_RegexCompiled | 120,022.3 ns
+              = mean 121749.6 ns (1 Nanosecond (0.000000001 sec))
      */
     /*
-    |                              Method |         Mean |       Error |      StdDev |       Median |
-    |------------------------------------ |-------------:|------------:|------------:|-------------:|
-    |                              Regex1 |   9,964.7 ns |    42.90 ns |    38.03 ns |   9,967.4 ns |
-    |                      Regex2Compiled |   4,036.1 ns |    27.87 ns |    24.71 ns |   4,028.9 ns |
-    |                              Regex3 |   9,928.7 ns |    50.62 ns |    44.87 ns |   9,926.6 ns |
-    |                              Regex4 |   4,606.4 ns |    39.53 ns |    36.98 ns |   4,599.8 ns |
-    |                              Regex5 |   1,774.9 ns |    12.26 ns |    11.47 ns |   1,776.1 ns |
-    |                  Regex5MatchSuccess |   1,861.6 ns |    11.82 ns |    11.05 ns |   1,860.7 ns |
-    |              Regex6CultureInvariant |   1,770.9 ns |     9.70 ns |     9.07 ns |   1,774.6 ns |
-    |                              Regex7 |   1,777.1 ns |    10.40 ns |     8.68 ns |   1,780.1 ns |
-    |                              Regex8 |   1,965.2 ns |    37.30 ns |    36.64 ns |   1,964.5 ns |
-    |                              Regex9 |  27,638.2 ns |   594.20 ns | 1,009.00 ns |  27,095.6 ns |
+    |                                   Method |         Mean |       Error |      StdDev |
+    |----------------------------------------- |-------------:|------------:|------------:|
+    |                                   Regex1 |   9,750.8 ns |    46.47 ns |    41.19 ns |
+    |                           Regex2Compiled |   3,937.7 ns |    23.44 ns |    21.93 ns |
+    |                                   Regex3 |   9,589.3 ns |    37.16 ns |    34.76 ns |
+    |                                   Regex4 |   4,423.9 ns |     9.74 ns |     9.11 ns |
+    |                           Regex5Compiled |   1,793.1 ns |     5.01 ns |     4.19 ns |
+    |               Regex5CompiledMatchSuccess |   1,764.2 ns |     1.50 ns |     1.33 ns |
+    |           Regex6CompiledCultureInvariant |   1,762.0 ns |     4.17 ns |     3.70 ns |
+    |                           Regex7Compiled |   1,727.3 ns |     2.90 ns |     2.57 ns |
+    |           Regex8CompiledCultureInvariant |   1,839.2 ns |     1.96 ns |     1.53 ns |
+    |           Regex9CompiledCultureInvariant |  27,362.8 ns |    79.46 ns |    66.35 ns |
 
-    |            RegXwildExtLambdaConariL |   1,343.1 ns |     4.81 ns |     4.50 ns |   1,342.3 ns |
-    |                RegXwildExtDllImport |   1,611.4 ns |     7.95 ns |     6.64 ns |   1,612.8 ns |
-    |            RegXwildEssLambdaConariL |   1,770.4 ns |    15.08 ns |    11.77 ns |   1,772.1 ns |
-    |                RegXwildEssDllImport |   2,053.8 ns |     5.92 ns |     5.25 ns |   2,054.3 ns |
-    |          RegXwildMatchLambdaConariL |   1,741.1 ns |     7.54 ns |     6.29 ns |   1,743.1 ns |
-    |          RegXwildMatchFastDllImport |   1,693.0 ns |    10.63 ns |     9.42 ns |   1,694.2 ns |
-    |      RegXwildMatchFastLambdaConariL |   1,506.2 ns |     5.65 ns |     5.28 ns |   1,507.4 ns |
-    |            RegXwildExtFastDllImport |   1,212.2 ns |     7.65 ns |     6.78 ns |   1,212.4 ns |
-    |        RegXwildExtFastLambdaConariL |   1,083.9 ns |     6.45 ns |     6.03 ns |   1,085.7 ns |
+    |                RegXwild_Conari_LambdaExt |   1,281.7 ns |     2.15 ns |     1.80 ns |
+    |                RegXwild_Conari_LambdaEss |   1,700.5 ns |     4.90 ns |     4.09 ns |
+    |              RegXwild_Conari_LambdaMatch |   1,660.0 ns |     4.99 ns |     4.43 ns |
 
-    |        PrepareOnce_RegexNonCompiled |   4,964.5 ns |    25.28 ns |    23.65 ns |   4,962.4 ns |
-    |           PrepareOnce_RegexCompiled | 128,166.4 ns |   837.10 ns |   783.03 ns | 128,256.5 ns |
-    |  PrepareOnce_RegexCompiledInvariant | 124,633.1 ns |   546.17 ns |   510.88 ns | 124,676.8 ns |
+    |             PrepareOnce_RegexNonCompiled |   4,840.4 ns |    28.19 ns |    26.37 ns |
+    |                PrepareOnce_RegexCompiled | 120,022.3 ns | 1,337.22 ns | 1,250.83 ns |
+    |       PrepareOnce_RegexCompiledInvariant | 119,583.8 ns |   342.04 ns |   319.94 ns |
 
-    |                  PrepareOnce_Conari |     436.4 ns |     1.21 ns |     1.07 ns |     436.6 ns |
-    | PrepareOnce_DLL_LoadingAndUnloading | 229,167.7 ns | 1,094.90 ns |   914.29 ns | 229,245.1 ns |
-    |     PrepareOnce_ConariFastDllImport |     665.6 ns |     3.03 ns |     2.68 ns |     666.1 ns |
-    |              PrepareOnce_ConariFast |     395.3 ns |     0.66 ns |     0.62 ns |     395.4 ns |
+    |               PrepareOnce_ConariDelegate |     499.7 ns |     2.08 ns |     1.95 ns |
+    |      PrepareOnce_Dll_LoadingAndUnloading |  75,152.1 ns |   250.67 ns |   222.22 ns |
+    | PrepareOnce_Dll_NoPE_LoadingAndUnloading |  18,598.1 ns |    72.37 ns |    67.69 ns |
+
+    |           Prepare_Conari_StrTxtAllocFree |     289.6 ns |     0.76 ns |     0.71 ns |
+    |   PrepareOnce_Conari_StrPatternAllocFree |     283.6 ns |     0.88 ns |     0.82 ns |
     */
+    /*
+     * In the end means,
+     * 
+                  Core              |    First match  |     First ten    |  First hundred  |   First thousand  
+        ----------------------------|-----------------|------------------|-----------------|-----------------
+        .NET regex engine Compiled  |    121,749.6 ns |     137,295.3 ns |    292,752.3 ns |   1,847,322.3 ns
+        .NET regex engine           |      9,264.3 ns |      49,079.4 ns |    447,230.4 ns |   4,428,740.4 ns
+        regXwild Ess via Conari     |     21,331.0 ns |      38,877.4 ns |    214,341.4 ns |   1,968,981.4 ns
+        regXwild Ext via Conari     |     20,952.7 ns |      35,094.4 ns |    176,511.4 ns |   1,590,681.4 ns 
+     */
     public class Regexes
     {
         private const string RP1 = "Task \".*?\" skipped, due to false condition; (.*?) was evaluated as (.*?).";
         private const string RP2 = @"Task "".*?"" skipped, due to false condition; \(.*?\) was evaluated as \(.*?\)\.";
         private const string RP3 = @"Task ""[^""]+?"" skipped, due to false condition; \(.*?\) was evaluated as \(.*?\)\.";
+
         // ((<->)->) based on https://github.com/3F/E-MSBuild/blob/9cd4453e2fc64f8bfd2d66fd5213ce54dc4b5d43/E-MSBuild/Pattern.cs#L208
         private const string RP4 = @"Task "".*?"" skipped, due to false condition; \(((?>[^\(\)]|\((?<R>)|\)(?<-R>))*(?(R)(?!)))\) was evaluated as \(((?>[^\(\)]|\((?<R>)|\)(?<-R>))*(?(R)(?!)))\)\.";
 
@@ -64,71 +71,14 @@ namespace Tests
         private const string TXT = @"Task ""Error"" skipped, due to false condition; (('$(CurrentSolutionConfigurationContents)' == '') and ('$(SkipInvalidConfigurations)' != 'true')) was evaluated as (('<SolutionConfiguration>('' != 'true')).";
         private Regex regex1, regex2, regex3, regex4, regex5, regex6, regex7, regex8, regex9;
 
-        #region regXwild via Conari lambda
-
-        private const string RXW_DLL = @"..\..\..\..\regXwild\bin\regXwild.dll";
+        private const string RXW_DLL = @"..\..\..\..\Tests\bin\Release\net472\x32\regXwild.dll";
         private string rxwDllResolvedPath;
 
-        private ConariL l;
+        private ConariL<CharPtr> l;
+        private CharPtr uText, uPattern;
 
-#if !CONARI_1_5
-        private UnmanagedString uText;      // obsolete
-        private UnmanagedString uPattern;   // obsolete
-#else
-        private NativeString<CharPtr> uText;
-        private NativeString<CharPtr> uPattern;
-#endif
-
-        private Func<CharPtr, CharPtr, bool, bool> rxwEssC;
-        private Func<CharPtr, CharPtr, bool, bool> rxwExtC;
+        private Func<CharPtr, CharPtr, bool, bool> rxwExtC, rxwEssC;
         private Func<CharPtr, CharPtr, ulong, bool> rxwMatch;
-
-        private Func<CharPtr, bool> rxwMatchFast;
-        private Action<CharPtr> rxwMatchInit;
-        private Action rxwMatchClose;
-
-        private Func<CharPtr, bool> rxwExtFast;
-        private Action<CharPtr> rxwExtInit;
-        private Action rxwExtClose;
-
-        #endregion
-
-        #region regXwild via DllImport
-        
-        #pragma warning disable IDE1006 // Naming Styles
-        private static class NativeMethods
-        {
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool searchEssC([MarshalAs(UnmanagedType.LPStr)] string data, [MarshalAs(UnmanagedType.LPStr)] string filter, bool icase = false);
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool searchExtC([MarshalAs(UnmanagedType.LPStr)] string data, [MarshalAs(UnmanagedType.LPStr)] string filter, bool icase = false);
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool match([MarshalAs(UnmanagedType.LPStr)] string data, [MarshalAs(UnmanagedType.LPStr)] string pattern, uint options, IntPtr m);
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            public static extern void matchInit([MarshalAs(UnmanagedType.LPStr)] string data);
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            public static extern void matchClose();
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool matchFast([MarshalAs(UnmanagedType.LPStr)] string pattern);
-
-            [DllImport(RXW_DLL, CharSet = CharSet.Ansi)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool extFast([MarshalAs(UnmanagedType.LPStr)] string pattern);
-        }
-#pragma warning restore IDE1006 // Naming Styles
-
-        #endregion
-
-        #region svc
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -145,209 +95,124 @@ namespace Tests
             regex8 = new Regex(RP3, RegexOptions.Compiled | RegexOptions.CultureInvariant);
             regex9 = new Regex(RP4, RegexOptions.Compiled | RegexOptions.CultureInvariant);
 
-            l = new ConariL(rxwDllResolvedPath);
+            l = new ConariL<CharPtr>(new Config(rxwDllResolvedPath) { PeImplementation = PeImplType.Disabled }, null);
 
-#if !CONARI_1_5
-            uText       = new UnmanagedString(TXT, UnmanagedString.SType.Ansi); // obsolete
-            uPattern    = new UnmanagedString(WP1, UnmanagedString.SType.Ansi); // obsolete
-#else
-            uText       = l.Strings.cstr<CharPtr>(TXT); // or l._T(TXT)
-            uPattern    = l.Strings.cstr<CharPtr>(WP1); // or l._T(pattern)
-#endif
+            uText       = l._T(TXT);
+            uPattern    = l._T(WP1);
 
-            // NOTE: Conari's DLR (ConariX, or 1.5+ (IDlrAccessor)ConariL, and ConariX.Make(new(RXW_X), out dynamic), ...)
-            // Eg.   using dynamic l = new Conarix(...);
-            //       l.match<bool>("", "", 0); // but you have to pay for the convenience in one-line use
+            // NOTE: We can call regXwild without any declarations at all using Conari DLR version
+            // For example, l._.match<bool>(uText, uPattern, 0L); // (IDlrAccessor)ConariL
+            // Or using ConariX
+            // using(dynamic x = new ConariX(rxwDllResolvedPath)) { x.match<bool>(uText, uPattern, 0L); } etc.
+            // but this will add at least ~19 us for generating related data at runtime
 
-            // thus, focus on lambda way
-
-            rxwEssC     = l.bindFunc<Func<CharPtr, CharPtr, bool, bool>>("searchEssC");
-            rxwExtC     = l.bindFunc<Func<CharPtr, CharPtr, bool, bool>>("searchExtC");
-            rxwMatch    = l.bindFunc<Func<CharPtr, CharPtr, ulong, bool>>("match");
-
-            rxwMatchFast    = l.bindFunc<Func<CharPtr, bool>>("matchFast");
-            rxwMatchInit    = l.bindFunc<Action<CharPtr>>("matchInit");
-            rxwMatchClose   = l.bindFunc<Action>("matchClose");
-
-            rxwMatchInit(uText);
-
-            rxwExtFast  = l.bindFunc<Func<CharPtr, bool>>("extFast");
-            rxwExtInit  = l.bindFunc<Action<CharPtr>>("extInit");
-            rxwExtClose = l.bindFunc<Action>("extClose");
-
-            rxwExtInit(uText);
+            // That's why here's lambda version, semi-automatic way.
+            rxwMatch    = l.bind<Func<CharPtr, CharPtr, ulong, bool>>("match");
+            rxwExtC     = l.bind<Func<CharPtr, CharPtr, bool, bool>>("searchExtC");
+            rxwEssC     = l.bind<Func<CharPtr, CharPtr, bool, bool>>("searchEssC");
         }
 
         [GlobalCleanup]
         public void GlobalCleanup()
         {
-#if !CONARI_1_5
-            uText?.Dispose();
-            uPattern?.Dispose();
-#endif
-            rxwMatchClose();
-            rxwExtClose();
-            l?.Dispose();
+            l.Dispose();
         }
+
+        #region .NET regex
+
+        [Benchmark]
+        public void Regex1() => _ = regex1.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex2Compiled() => _ = regex2.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex3() => _ = regex3.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex4() => _ = regex4.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex5Compiled() => _ = regex5.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex5CompiledMatchSuccess() => _ = regex5.Match(TXT).Success;
+
+        [Benchmark]
+        public void Regex6CompiledCultureInvariant() => _ = regex6.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex7Compiled() => _ = regex7.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex8CompiledCultureInvariant() => _ = regex8.IsMatch(TXT);
+
+        [Benchmark]
+        public void Regex9CompiledCultureInvariant() => _ = regex9.IsMatch(TXT);
 
         #endregion
 
+        #region regXwild via Conari lambda version
+
         [Benchmark]
-        public void Regex1()
+        public void RegXwild_Conari_LambdaExt() => _ = rxwExtC(uText, uPattern, false);
+
+        [Benchmark]
+        public void RegXwild_Conari_LambdaEss() => _ = rxwEssC(uText, uPattern, false);
+
+        [Benchmark]
+        public void RegXwild_Conari_LambdaMatch() => _ = rxwMatch(uText, uPattern, 0);
+
+        #endregion
+
+        #region Preparing for regex
+
+        [Benchmark]
+        public void PrepareOnce_RegexNonCompiled() => _ = new Regex(RP2);
+
+        [Benchmark]
+        public void PrepareOnce_RegexCompiled() => _ = new Regex(RP2, RegexOptions.Compiled);
+
+        [Benchmark]
+        public void PrepareOnce_RegexCompiledInvariant() => _ = new Regex(RP2, RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        #endregion
+
+        #region Preparing for Conari
+
+        [Benchmark]
+        public void PrepareOnce_ConariDelegate() => _ = l.bind<Func<CharPtr, CharPtr, ulong, bool>>("match");
+
+        [Benchmark]
+        public void PrepareOnce_Dll_LoadingAndUnloading()
         {
-            _ = regex1.IsMatch(TXT);
+            using(var l = new ConariL<CharPtr>(rxwDllResolvedPath)) { }
         }
 
         [Benchmark]
-        public void Regex2Compiled()
+        public void PrepareOnce_Dll_NoPE_LoadingAndUnloading()
         {
-            _ = regex2.IsMatch(TXT);
+            using(var l = new ConariL<CharPtr>(new Config(rxwDllResolvedPath, false)
+            {
+                PeImplementation = PeImplType.Disabled
+            }))
+            {
+
+            }
         }
 
         [Benchmark]
-        public void Regex3()
+        public void Prepare_Conari_StrTxtAllocFree()
         {
-            _ = regex3.IsMatch(TXT);
+            new NativeString<CharPtr>(TXT.Length).Dispose();
         }
 
         [Benchmark]
-        public void Regex4()
+        public void PrepareOnce_Conari_StrPatternAllocFree()
         {
-            _ = regex4.IsMatch(TXT);
+            new NativeString<CharPtr>(WP1.Length).Dispose();
         }
 
-        [Benchmark]
-        public void Regex5()
-        {
-            _ = regex5.IsMatch(TXT);
-        }
-
-        [Benchmark]
-        public void Regex5MatchSuccess()
-        {
-            _ = regex5.Match(TXT).Success;
-        }
-
-        [Benchmark]
-        public void Regex6CultureInvariant()
-        {
-            _ = regex6.IsMatch(TXT);
-        }
-
-        [Benchmark]
-        public void Regex7()
-        {
-            _ = regex7.IsMatch(TXT);
-        }
-
-        [Benchmark]
-        public void Regex8()
-        {
-            _ = regex8.IsMatch(TXT);
-        }
-
-        [Benchmark]
-        public void Regex9()
-        {
-            _ = regex9.IsMatch(TXT);
-        }
-
-        [Benchmark]
-        public void RegXwildExtLambdaConariL()
-        {
-            _ = rxwExtC(uText, uPattern, false);
-        }
-
-        [Benchmark]
-        public void RegXwildExtDllImport()
-        {
-            _ = NativeMethods.searchExtC(TXT, WP1, false);
-        }
-
-        [Benchmark]
-        public void RegXwildEssLambdaConariL()
-        {
-            _ = rxwEssC(uText, uPattern, false);
-        }
-
-        [Benchmark]
-        public void RegXwildEssDllImport()
-        {
-            _ = NativeMethods.searchEssC(TXT, WP1, false);
-        }
-
-        [Benchmark]
-        public void RegXwildMatchLambdaConariL()
-        {
-            _ = rxwMatch(uText, uPattern, 0);
-        }
-
-        [Benchmark]
-        public void RegXwildMatchFastDllImport()
-        {
-            _ = NativeMethods.matchFast(WP1);
-        }
-
-        [Benchmark]
-        public void RegXwildMatchFastLambdaConariL()
-        {
-            _ = rxwMatchFast(uPattern);
-        }
-
-        [Benchmark]
-        public void RegXwildExtFastDllImport()
-        {
-            _ = NativeMethods.extFast(WP1);
-        }
-
-        [Benchmark]
-        public void RegXwildExtFastLambdaConariL()
-        {
-            _ = rxwExtFast(uPattern);
-        }
-
-        [Benchmark]
-        public void PrepareOnce_RegexNonCompiled()
-        {
-            _ = new Regex(RP2);
-        }
-
-        [Benchmark]
-        public void PrepareOnce_RegexCompiled()
-        {
-            _ = new Regex(RP2, RegexOptions.Compiled);
-        }
-
-        [Benchmark]
-        public void PrepareOnce_RegexCompiledInvariant()
-        {
-            _ = new Regex(RP2, RegexOptions.Compiled | RegexOptions.CultureInvariant);
-        }
-
-        [Benchmark]
-        public void PrepareOnce_Conari()
-        {
-            _ = l.bindFunc<Func<CharPtr, CharPtr, bool, bool>>("searchEssC");
-        }
-
-        [Benchmark]
-        public void PrepareOnce_DLL_LoadingAndUnloading()
-        {
-            using(var l = new ConariL(rxwDllResolvedPath)) { }
-        }
-
-        [Benchmark]
-        public void PrepareOnce_ConariFastDllImport()
-        {
-            NativeMethods.matchInit(TXT);
-            NativeMethods.matchClose();
-        }
-
-        [Benchmark]
-        public void PrepareOnce_ConariFast()
-        {
-            rxwMatchInit(uText);
-            rxwMatchClose();
-        }
+        #endregion
     }
 }
